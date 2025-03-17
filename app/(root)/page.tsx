9e1975fe516fdeb4d2897f40516e70db71f83732
tharuunnn <tharun220804@gmail.com>
2025-03-17 13:10:44 +0530

    imported sample books

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
     <div>
-      <Button>click me!</Button>
+      <BookOverview/>
+      <BookList/>
     </div>
   );
 };
---
+++ components/BookList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <section>
+      <div>
+        <h2 className="font-bebas-neue text-4xl text-light-100">
+          Popular books
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default BookList;
