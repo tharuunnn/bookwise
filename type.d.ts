4ddc00b697089bdac4243e56863323ae2b60a7ba
tharuunnn <tharun220804@gmail.com>
2025-03-18 18:23:37 +0530

    hero section

---
+++ type.d.ts
@@ -0,0 +1,15 @@
+interface BooK {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  totalCopies: number;
+  availableCopies: number;
+  description: string;
+  coverColor: string;
+  coverUrl: string;
+  videoUrl: string;
+  summary: string;
+  isLoanedBook?: boolean;
+}
