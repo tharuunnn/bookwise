ab3438024998c6dccc2dec4e4d694ffe98422864
tharuunnn <tharun220804@gmail.com>
2025-03-31 20:38:07 +0530

    setting up for workflows with qstash

--- lib/config.ts
+++ lib/config.ts
@@ -14,6 +14,8 @@ const config = {
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_URL!,
       redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
     },
   },
 };
---
+++ lib/workflow.ts
@@ -0,0 +1,6 @@
+import { Client as WorkflowClient } from "@upstash/workflow";
+import config from "./config";
+
+export const workflowClient = new WorkflowClient({
+  baseUrl: config.env.upstash.qstashUrl,
+});
